668ad9fcc9bdf921b5766f6d285703073a5eccec
iemwill <hello@laubenheimer.eu>
2021-08-03 18:14:31 +0200

    improve registration cycle

--- src/Components/Opener.js
+++ src/Components/Opener.js
@@ -12,48 +12,54 @@ class Opener extends Component {
     };
   }
   async register(ip) {
-    try {
-        const sourceAccount = '0x77754bDDA8a6391f340Bb2ffe2DA6A58A30B7228';
-        const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/4fdbae7ae3e94fb9a3033c623fc4e7f0"));
-        const consumerAddress = '0x91B245abbDC633A0A7dDC25316476e176D42DCc0';
-        const consumerContract = new web3.eth.Contract(consumerAbi, consumerAddress);
-        const myData = consumerContract.methods.register(ip).encodeABI();
-        const txCount = await web3.eth.getTransactionCount(sourceAccount);
-        // Build the transaction
-        const txObject = {
-            nonce: web3.utils.toHex(txCount),
-            to: consumerAddress,
-            from: sourceAccount,
-            chainId: 3,
-            value: web3.utils.toHex(web3.utils.toWei('0', 'ether')),
-            gasLimit: web3.utils.toHex(210000),
-            gasPrice: web3.utils.toHex(web3.utils.toWei('2', 'gwei')),
-            data: myData  
-        };
-        // Sign the transaction
-        const raw = await web3.eth.accounts.signTransaction(
-            txObject,
-            process.env.REACT_APP_SECRET
-        );
-        // Broadcast the transaction
-        const transaction = await web3.eth.sendSignedTransaction(raw.rawTransaction);
-        console.log('TX: ', transaction);
-    } catch (error) {
-      console.log('FAIL: ', error);
+    if (this.state.count == 0) {
+      try {
+          const sourceAccount = '0x77754bDDA8a6391f340Bb2ffe2DA6A58A30B7228';
+          const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/4fdbae7ae3e94fb9a3033c623fc4e7f0"));
+          const consumerAddress = '0x91B245abbDC633A0A7dDC25316476e176D42DCc0';
+          const consumerContract = new web3.eth.Contract(consumerAbi, consumerAddress);
+          const myData = consumerContract.methods.register(ip).encodeABI();
+          const txCount = await web3.eth.getTransactionCount(sourceAccount);
+          // Build the transaction
+          const txObject = {
+              nonce: web3.utils.toHex(txCount),
+              to: consumerAddress,
+              from: sourceAccount,
+              chainId: 3,
+              value: web3.utils.toHex(web3.utils.toWei('0', 'ether')),
+              gasLimit: web3.utils.toHex(210000),
+              gasPrice: web3.utils.toHex(web3.utils.toWei('2', 'gwei')),
+              data: myData  
+          };
+          // Sign the transaction
+          const raw = await web3.eth.accounts.signTransaction(
+              txObject,
+              process.env.REACT_APP_SECRET
+          );
+          // Broadcast the transaction
+          const transaction = await web3.eth.sendSignedTransaction(raw.rawTransaction);
+          console.log('TX: ', transaction);
+      } catch (error) {
+        console.log('Registration failed: ', error);
+      }
     }
   }
   render() {
-  publicIP()
-    .then(ip => {
-      const count = 1;
-      this.setState({count, ip});
-      this.register(ip);
-    })
-    .catch(error => {
-      console.log('IP :', error);
-      this.register('HiddenIP');
-      // 'Unable to get IP address.'
-    });
+    //Get IP and register at a smart contract
+    if (this.state.count == 0) {
+      publicIP()
+        .then(ip => {
+          this.register(ip);
+          const count = 1;
+          this.setState({count, ip});
+        })
+        .catch(error => {
+          this.register('HiddenIP');
+          const count = 1;
+          this.setState({count});
+          console.log('IP-error :', error);
+        });
+    }
     return (
       <section id="opener">
         <div className="opener">
