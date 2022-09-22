59bdfeebd389c704d2b173a54d0cb6ee79888896
iemwill <hello@laubenheimer.eu>
2022-09-22 13:57:12 +0200

    #13 use all contracts on moonbase

--- src/Components/Backend/Cookies/addWebAppAction.js
+++ src/Components/Backend/Cookies/addWebAppAction.js
@@ -3,8 +3,8 @@ import cookieAbi from './Cookies.json';
 async function addWebAppAction(ip, buttonID, sessionID) {
 	try {
 		const sourceAccount = '0x77754bdda8a6391f340bb2ffe2da6a58a30b7228';
-		const web3 = new Web3(new Web3.providers.HttpProvider(process.env.REACT_APP_API_KEY));
-		const contractAddress = '0xd2cf9f677f361f23c576825978338c4a21291646';
+		const web3 = new Web3('https://rpc.api.moonbase.moonbeam.network');
+		const contractAddress = '0x48bfb22ebb07d897ceb7b7328400ba3f1651f0ae';
 		const cookieContract = new web3.eth.Contract(cookieAbi, contractAddress);
 		const myData = cookieContract.methods.webAppAction(ip, web3.utils.toHex(buttonID), web3.utils.toHex(sessionID)).encodeABI();
 		const txCount = await web3.eth.getTransactionCount(sourceAccount);
@@ -16,7 +16,7 @@ async function addWebAppAction(ip, buttonID, sessionID) {
           	from: sourceAccount,
           	chainId: networkId,
           	value: web3.utils.toHex(web3.utils.toWei('0', 'ether')),
-          	gasLimit: web3.utils.toHex(100000),
+          	gasLimit: web3.utils.toHex(73000),
           	//gasPrice: web3.utils.toHex(web3.utils.toWei('2', 'Gwei')),
           	data: myData,
             maxPriorityFeePerGas: web3.utils.toHex(web3.utils.toWei('1', 'Gwei')),
