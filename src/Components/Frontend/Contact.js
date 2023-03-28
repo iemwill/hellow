aec20e730c5e9f09474687e9c6c719ce795e938f
iemwill <hello@laubenheimer.eu>
2023-03-28 10:11:47 +0200

    Add Google SEO & frontend enhancements

--- src/Components/Frontend/Contact.js
+++ src/Components/Frontend/Contact.js
@@ -30,33 +30,33 @@ class Contact extends Component {
         <h3>
           Congratulations! You have created an address for ethereum and minted a NFT<br/><br/>
           The private key to manifest ownership will show up below, best praxis is to write it on a paper<br/><br/>
-          For workshop & project requests as well as how to claim the newly generated NFT & ERC20-tokens 
-          write an email via the<br/><br/>
+          For workshop & project requests as well as how to claim the newly generated NFT & ERC20-Tokens 
+          contact us via the
+          <br/><br/>
           <a href="mailto:hello@laubenheimer.eu"
-          onClick={() => this._addWebAppAction(9)}> 'Inspirator & Consultent' - button</a>
-          <br/><br/><br/><br/><br/>
-          {this.props.seed}
+          onClick={() => this._addWebAppAction(6)}> 'Inspirator & Consultent' - button</a>
+          <br/><br/><br/>
+          <h4>{this.props.seed}</h4>
         </h3>
     } else {
       this.state.text = 
         <h3>
           Congratulations! You have minted a 
             <a href="https://moonbase.moonscan.io/address/0xc3e13D5E3e8fFa7E601f657Fd690AF70E224d1a5"
-              onClick={() => this._addWebAppAction(10)} target="_blank" rel="noreferrer">Website NFT</a> 
+              onClick={() => this._addWebAppAction(7)} target="_blank" rel="noreferrer">Website NFT</a> 
           on the moonbase test-network<br/><br/>
-          The NFT will be accessible through your wallet provider and every click-action on this site will create ERC20-token<br/><br/>
-          To enhance your knowledge in web3, blockchain technology and solidity write a workshop request via the<br/><br/>
+          The NFT will be accessible through your wallet provider. Every click-action on this site will create ERC20-token<br/><br/>
+          To enhance & exchange your knowledge in web3, blockchain technology, solidity and for project requests contact us via the<br/><br/>
           <a href="mailto:hello@laubenheimer.eu"
-          onClick={() => this._addWebAppAction(9)}>'Inspirator & Consultent' - button</a>
-          <br/><br/><br/><br/><br/>
+          onClick={() => this._addWebAppAction(6)}>'Inspirator & Consultent' - button</a>
+          <br/><br/><br/>
           {this.props.customerAccount}
         </h3>
     }
     return (
       <section id="contact">
-        <div className="contact">
+        <div id="contact-top">
           <h2>Analysis | Consultancy | Design | Development | Education </h2>
-          <br/><br/><br/><br/><br/>
           <a href="https://wa.me/message/AAIDX7NDPAJ3J1" target="_blank" rel="noreferrer"
             onClick={() => this._addWebAppAction(1)}><FaWhatsapp/></a>
           <a href="https://twitter.com/iem_wll" target="_blank" rel="noreferrer"
@@ -67,16 +67,10 @@ class Contact extends Component {
             onClick={() => this._addWebAppAction(4)}><FaTelegram/></a>
           <a href="https://linkedin.com/in/w-ll" target="_blank" rel="noreferrer"
             onClick={() => this._addWebAppAction(5)}><FaLinkedin/></a>
-          <br/><br/><br/><br/>
         </div>
         <div id="c0n74c7">
           <a href="mailto:hello@laubenheimer.eu" 
-            onClick={() => this._addWebAppAction(6)}><h3>Inspirator</h3></a>
-          <a href="mailto:hello@laubenheimer.eu"
-            onClick={() => this._addWebAppAction(7)}><h3> & </h3></a>
-          <a href="mailto:hello@laubenheimer.eu"
-            onClick={() => this._addWebAppAction(8)}><h3>Consultent</h3></a>
-          <br/><br/><br/><br/>
+            onClick={() => this._addWebAppAction(6)}><h3>Inspirator<br/>&<br/>Consultent</h3></a>
         </div>
         <div id="text">
           {this.state.text}
